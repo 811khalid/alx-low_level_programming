f7fb9ccb9b4747cdc1bb5e116938bd6bc739adc7
811khalid <khalidtewfiq151@gmail.com>
2023-12-13 23:40:40 -0800

    first push for files

---
+++ 0x18-dynamic_libraries/2-strlen.c
@@ -0,0 +1,16 @@
+#include"main.h"
+/**
+  * _strlen - returns the length of a string
+  * @s: string to be tested
+  * Return: length of the string
+  */
+
+int _strlen(char *s)
+{
+int i = 0;
+while (*(s + i) != 00)
+{
+i++;
+}
+return (i);
+}
